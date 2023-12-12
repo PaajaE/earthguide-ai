77d325571b53435473402f2e1a142a4f0b27c913
Pavel Ečer <ecerpavel@gmail.com>
2023-12-12 13:29:02 +0100

    GA init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,21 @@
+import Script from 'next/script'
 import Main from '@/components/main'
 import { airlinesData } from '@/utils/data/airlines';
 
 export default function Home() {
   return (
-    <Main airlineData={airlinesData.default} />
+    <>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-JFYCT54QGC" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-JFYCT54QGC');
+        `}
+      </Script>
+      <Main airlineData={airlinesData.default} />
+    </>
   );
 }
